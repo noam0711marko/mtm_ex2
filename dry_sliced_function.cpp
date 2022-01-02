9bf6777d405d0cf78bec3c0cad660cad758d32b1
Noam Marko <noammarko@Noams-MacBook-Pro.local>
2022-01-02 13:04:30 +0200

    dry part done

--- dry_sliced_function.cpp
+++ dry_sliced_function.cpp
@@ -7,10 +7,12 @@
 
 using std::cout;
 
+class BadInput{};
+
 template <class T>
 std::vector<T> slice(std::vector<T> vec, int start, int step, int stop){
     if(start<0 || start>=vec.size() || stop <0 || stop >= vec.size() || step<=0){
-        /*throw badInput; //update this */
+        throw BadInput();
     }
     std::vector<T> new_vec={};
     if (start>=stop){
