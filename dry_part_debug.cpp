155244d2845ee37f54bed539244bc27e0b5fa440
Noam Marko <noammarko@Noams-MacBook-Pro.local>
2022-01-02 12:55:33 +0200

    should work

--- dry_part_debug.cpp
+++ dry_part_debug.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include "vector"
 
+class BadInput{};
+
 template<class T>
         std::vector<T> slice(std::vector<T> vec, int start, int step, int stop);
 
@@ -11,16 +13,13 @@ template<class T>
 
 class A {
 public:
-    std::vector<int*> values;
-    void add(int x) { values.push_back(new int(x)); }
-    ~A();
-};
-A::~A() {
-    int len=values.size();
-    for (int i=0; i<len; ++i){
-        delete(values[i]);
+    std::vector<std::shared_ptr<int>> values;
+    void add(int x){
+        std::shared_ptr<int> value_pointer(new int(x));
+        values.push_back(value_pointer);
     }
-}
+};
+
 int main() {
     A a, sliced;
     a.add(0); a.add(1); a.add(2); a.add(3); a.add(4); a.add(5);
@@ -34,7 +33,7 @@ int main() {
 template<class T>
 std::vector<T> slice(std::vector<T> vec, int start, int step, int stop){
     if(start<0 || start>=vec.size() || stop <0 || stop >= vec.size() || step<=0){
-        /*throw badInput; //update this */
+        throw BadInput();
     }
     std::vector<T> new_vec={};
     if (start>=stop){
