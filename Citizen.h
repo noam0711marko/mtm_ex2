7a4586c2ed20cb1e6e53c8ed50d4da0a94c488e6
Noam Marko <noammarko@Noams-MacBook-Pro.local>
2022-01-14 18:13:13 +0200

    citizen h documentation done

--- Citizen.h
+++ Citizen.h
@@ -20,7 +20,7 @@ using std::ostream;
  *   ~Citizen                   - Virtual default destructor for citizen.
  *   Citizen                    - Default Copy Constructor for citizen.
  *   Operator=                  - Default Assignment Operator for citizen.
- *   getId                      - Returns the id of a citizen.
+ *   getId                      - Returns the ID of a citizen.
  *   getFirstName               - Returns the first name of a citizen.
  *   getLastName                - Returns the last name of a citizen.
  *   getBirthYear               - Returns the year of birth of a citizen.
@@ -49,55 +49,225 @@ namespace mtm {
          * @param new_first_name - Target first name of the citizen.
          * @param new_last_name - Target last name of the citizen.
          * @param new_year - Target year of birth of the citizen.
-         *
-         * @return
-         *      A new citizen with the inserted details
          */
         explicit Citizen(int new_id, string new_first_name, string new_last_name, int new_year);
 
-        Citizen(const Citizen &) = default;
+        /*
+         * Citizen: Default Copy Constructor for citizen.
+         *
+         * @param - the citizen which is data is requested to be copied
+         *
+         * @return
+         *      A new citizen with the same details as the citizen which was sent to the function
+         */
+        Citizen(const Citizen&) = default;
 
+        /*
+         * operator=: Default Assignment Operator for citizen.
+         *
+         * @param - the citizen which is data is requested to be assigned to the new citizen
+         *
+         * @return
+         *      The same citizen, but with the same details as the citizen which was sent to the function
+         */
         Citizen &operator=(const Citizen &) = default;
 
+        /*
+         * ~Citizen: Virtual default destructor for citizen.
+         *
+         * note: It's a virtual method which will be overridden in the derived classes.
+         */
         virtual ~Citizen() = default;
 
+        /*
+         * getId: Returns the id of a citizen.
+         *
+         * @return
+         *      The ID of the citizen
+         */
         int getId() const;
 
+        /*
+         * getFirstName: Returns the first name of a citizen.
+         *
+         * @return
+         *      The first name of the citizen
+         */
         string getFirstName() const;
 
+        /*
+         * getLastName: Returns the last name of a citizen.
+         *
+         * @return
+         *      The last name of the citizen
+         */
         string getLastName() const;
 
+        /*
+         * getBirthYear: Returns the year of birth of a citizen.
+         *
+         * @return
+         *      The year of birth of the citizen
+         */
         int getBirthYear() const;
 
+        /*
+         * getSalary: Pure Virtual method which Returns the salary of a citizen.
+         *
+         * note: It's a pure virtual method, so it can be used only in derived classes!
+         *       Therefore, it's not implemented in this base abstract class.
+         *
+         * @return
+         *      The salary of the citizen
+         */
         virtual int getSalary() const = 0;
 
-        friend bool operator<(const Citizen &, const Citizen &);
+        /*
+         * operator<: Returns if one citizen's ID is smaller than the second's ID.
+         *
+         * @param citizen_1- the first citizen to compare.
+         * @param citizen_2- the second citizen to compare.
+         *
+         * @return
+         *      True - if first citizen's ID is smaller than second citizen's ID.
+         *      False - otherwise.
+         */
+        friend bool operator<(const Citizen &citizen_1, const Citizen &citizen_2);
 
-        friend bool operator==(const Citizen &, const Citizen &);
+        /*
+         * operator==: Returns if one citizen's ID is equal to the second's ID.
+         *
+         * @param citizen_1- the first citizen to compare.
+         * @param citizen_2- the second citizen to compare.
+         *
+         * @return
+         *      True - if first citizen's ID is equal to second citizen's ID.
+         *      False - otherwise.
+         */
+        friend bool operator==(const Citizen &citizen_1, const Citizen &citizen_2);
 
+        /*
+         * printShort: Prints short details about the citizen
+         *
+         * note: It's a pure virtual method, so it can be used only in derived classes!
+         *       Therefore, it's not implemented in this base abstract class.
+         *
+         * @param - output stream to print in to.
+         *
+         * @return
+         *      The output stream
+         */
         virtual ostream &printShort(ostream &) const = 0;
 
+        /*
+         * printLong: Prints long details about the citizen
+         *
+         * note: It's a pure virtual method, so it can be used only in derived classes!
+         *       Therefore, it's not implemented in this base abstract class.
+         *
+         * @param - output stream to print in to.
+         *
+         * @return
+         *      The output stream
+         */
         virtual ostream &printLong(ostream &) const = 0;
 
-        ostream &printFullName(ostream &out) const;
+        /*
+         * printFullName: Prints citizen's full name.
+         *
+         * @param - output stream to print in to.
+         *
+         * @return
+         *      The output stream
+         */
+        ostream &printFullName(ostream &) const;
 
-        ostream &printIdBirthYear(ostream &out) const;
+        /*
+         * printIdBirthYear: Prints citizen's id and year of birth.
+         *
+         * @param - output stream to print in to.
+         *
+         * @return
+         *      The output stream
+         */
+        ostream &printIdBirthYear(ostream &) const;
 
+        /*
+         * clone: Pure virtual method which creates a (deep) copy of a citizen.
+         *
+         * note: It's a pure virtual method, so it can be used only in derived classes!
+         *       Therefore, it's not implemented in this base abstract class.
+         *
+         * @return
+         *      A new citizen which is a copy of the citizen sent to the method.
+         */
         virtual Citizen *clone() const = 0;
     };
 
-    bool operator<=(const Citizen &, const Citizen &);
+    /*
+     * operator<=: Returns if one citizen's ID is smaller than or equal to the second's ID.
+     *
+     * @param citizen_1- the first citizen to compare.
+     * @param citizen_2- the second citizen to compare.
+     *
+     * @return
+     *      True - if first citizen's ID is smaller than or equal to second citizen's ID.
+     *      False - otherwise.
+     */
+    bool operator<=(const Citizen &citizen_1, const Citizen &citizen_2);
 
-    bool operator>(const Citizen &, const Citizen &);
+    /*
+     * operator>: Returns if one citizen's ID is bigger than the second's ID.
+     *
+     * @param citizen_1- the first citizen to compare.
+     * @param citizen_2- the second citizen to compare.
+     *
+     * @return
+     *      True - if first citizen's ID is bigger than second citizen's ID.
+     *      False - otherwise.
+     */
+    bool operator>(const Citizen &citizen_1, const Citizen &citizen_2);
 
-    bool operator>=(const Citizen &, const Citizen &);
+    /*
+     * operator>=: Returns if one citizen's ID is bigger than or equal to the second's ID.
+     *
+     * @param citizen_1- the first citizen to compare.
+     * @param citizen_2- the second citizen to compare.
+     *
+     * @return
+     *      True - if first citizen's ID is bigger than or equal to second citizen's ID.
+     *      False - otherwise.
+     */
+    bool operator>=(const Citizen &citizen_1, const Citizen &citizen_2);
 
-    bool operator!=(const Citizen &, const Citizen &);
+    /*
+     * operator<=: Returns if one citizen's ID is not equal to the second's ID.
+     *
+     * @param citizen_1- the first citizen to compare.
+     * @param citizen_2- the second citizen to compare.
+     *
+     * @return
+     *      True - if first citizen's ID is not equal to second citizen's ID.
+     *      False - otherwise.
+     */
+    bool operator!=(const Citizen &citizen_1, const Citizen &citizen_2);
 
+
+    /*
+     * cmp_citizens_shared_ptr:
+     * struct which contains only the operator() which compare between shared pointers to citizen by citizen's
+     * operator <.
+     * this struct is sent to the set which contains shared pointers to citizens, and it's necessary for it's sorting.
+     */
     struct cmp_citizens_shared_ptr {
         bool operator()(const shared_ptr<Citizen> &a, const shared_ptr<Citizen> &b) const { return (*a) < (*b); }
     };
 
+    /*
+     * cmp_citizens_ptr:
+     * struct which contains only the operator() which compare between pointers to citizen by citizen's operator <.
+     * this struct is sent to the set which contains pointers to citizens, and it's necessary for it's sorting.
+     */
     struct cmp_citizens_ptr {
         bool operator()(Citizen *a, Citizen *b) const { return (*a) < (*b); }
     };
